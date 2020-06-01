ca765d7cbd0d61225b1856b5ee9fd7dc75e8ae45
shubhammalhotra28 <56790284+shubhammalhotra28@users.noreply.github.com>
2020-05-31 20:39:32 -0400

    Rendering through templates

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,20 @@
 #from django.conf.urls import url
+from django.conf.urls import url
 from django.urls import path
 from . import views # setting the view . -> for current directory
 
 urlpatterns = [
-	
-	path('',views.index, name = 'index'), # default homepage
+
+	# /music/
+	url(r'^$',views.index,name='index'),
+	#path('',views.index, name = 'index'), # default homepage
+
+
+	# /music/712/
+
+	url(r'^(?P<album_id>[0-9]+)$',views.detail,name='detail'),
+
+
 
 ]
 
--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 #from django.shortcuts import render
-
+from .models import Album
+from django.template import loader
 # Create your views here.
 
 # take a req and send http response
@@ -7,5 +8,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
-	return HttpResponse("<h1> this is the music app homepage </h1>")
+	all_albums = Album.objects.all()
+	template = loader.get_template('music/index.html')
+	context = {
+		'all_albums': all_albums,
+	}
+	return HttpResponse(template.render(context,request))
+	#return HttpResponse(""<h1> this is the music app homepage </h1>)
+
+def detail(request, album_id):
+	return HttpResponse("<h2> Details for album id : </h2>"+ str(album_id))
 
